334e9aa9605aedd6a5e41390f06112ea4f77b7b8
Asindu Deshan <130549448+Asindudeshan@users.noreply.github.com>
2025-04-26 13:41:26 +0530

    user.java

---
+++ User.java
@@ -0,0 +1,78 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "email", length = 45)
+    private String email;
+
+    @Column(name = "Username", length = 45)
+    private String username;
+
+    @Column(name = "password", length = 45)
+    private String password;
+
+    @Column(name = "nickname", length = 45)
+    private String nickname;
+
+    @Column(name = "profile_pic_path", length = 45)
+    private String profilePicPath;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getProfilePicPath() {
+        return profilePicPath;
+    }
+
+    public void setProfilePicPath(String profilePicPath) {
+        this.profilePicPath = profilePicPath;
+    }
+
+}
