a4e07c2344d2c93a8f3fb7b26054190f1af4bab4
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-05-07 19:03:10 +0530

    Update MainServer.java

--- ChatApplication/ChatApplication/ChatApplication/src/main/java/com/chatapplication/server/MainServer.java
+++ ChatApplication/ChatApplication/ChatApplication/src/main/java/com/chatapplication/server/MainServer.java
@@ -1,20 +1,2 @@
-package com.chatapplication.server;
+package com.chatapplication.server.;
 
-import com.chatapplication.server.service.ChatServerImpl;
-import com.chatapplication.rmi.ChatService;
-
-import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
-
-public class MainServer {
-    public static void main(String[] args) {
-        try {
-            LocateRegistry.createRegistry(1099); // Start RMI registry
-            ChatService chatService = new ChatServerImpl();
-            Naming.rebind("ChatService", chatService);
-            System.out.println("✅ RMI ChatService is running...");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
