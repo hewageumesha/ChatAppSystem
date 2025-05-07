b7900e6927ad818a4f743dedc837b83f0ed4fb3c
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-05-07 19:04:57 +0530

    Update MainServer.java

--- ChatApplication/ChatApplication/ChatApplication/src/main/java/com/chatapplication/server/MainServer.java
+++ ChatApplication/ChatApplication/ChatApplication/src/main/java/com/chatapplication/server/MainServer.java
@@ -1,2 +1,14 @@
 package com.chatapplication.server.;
 
+import com.chatapplication.server.service.ChatServerImpl;
+import com.chatapplication.rmi.ChatService;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class MainServer {
+    public static void main(String[] args) {
+        
+    }
+}
+
